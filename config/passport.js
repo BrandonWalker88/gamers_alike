4a9f7ec942ff3b221883b8a8830023d6df71f088
Jason Ozulumba <cjozulumba@gmail.com>
2020-10-17 18:42:21 -0400

    ONE MORE TIME

--- config/passport.js
+++ config/passport.js
@@ -7,8 +7,8 @@ var db = require("../models");
 passport.use(
   new SteamStrategy(
     {
-      returnURL: "https://gamers-alike.herokuapp.com/sigin/auth/steam/return",
-      realm: "https://gamers-alike.herokuapp.com/sigin",
+      returnURL: "https://gamers-alike.herokuapp.com/signin/auth/steam/return",
+      realm: "https://gamers-alike.herokuapp.com/signin",
       apiKey: "D94580D5312585B718FA616F6F1CB4F0", //Jason's Dev API Key
     },
     function (identifier, profile, done) {
