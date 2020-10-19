241582222d887b48e6d0f9ede5d7af18238a27f7
Jon Peach <jonmakesitbetter@gmail.com>
2020-10-18 21:18:55 -0400

    Styled signup page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -262,30 +262,21 @@ module.exports = function (app) {
   // Plus you really don't need it be pulled out this way
   // just "user => res.render("target-handlebar-file" ex. "home", {name-of-extraction: user} ex. {userdata: user} )
   // If done this way remember to use {{#dataValues}}whatever data you are grabbing{{/dataValues}} to grab the real stuff
-  app.get("/home", function (req, res) {
+  app.get("/home", async function (req, res) {
+    console.log("Booyah, it works!");
     if (req.user) {
-      db.User.findOne({
+      const user = await db.User.findOne({
         where: {
           id: req.user.id,
         },
-      }).then((user) => {
-        const attributes = [];
-        attributes.push(
-          { attr1: user.user_attr1 },
-          { attr2: user.user_attr2 },
-          { attr3: user.user_attr3 },
-          { attr4: user.user_attr4 },
-          { attr5: user.user_attr5 },
-          { attr6: user.user_attr6 }
-        );
-
-        res.render("home", attributes);
-      });
+      })
+        const friends = await user.getFriends()
+         await res.render("testfriendslist", { user: friends });
     } else {
       res.status(401).redirect("/login");
     }
   });
-
+  
   // Simple version that does require {{#dataValues}} grab the dat in handlebars
   // But it does grab friend or potential user you want check out and give access to their data
   app.get("/friend", function (req, res) {
