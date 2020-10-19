db1ff669ab9aadae62dfc3a1edd9ac84e9b4f4c9
Jason Ozulumba <cjozulumba@gmail.com>
2020-10-19 00:23:07 -0400

    Here we go

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -12,7 +12,6 @@ module.exports = function (app) {
   // });
 
   app.get("/friend-page", function (req, res) {
-    
     res.render("friend-page");
   });
 
@@ -23,17 +22,16 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     // If the user already has an account send them to the home page
     if (req.user) {
-      res.redirect("/allUsers");
+      res.redirect("/home");
     } else {
       res.render("signup");
     }
-    
   });
 
   app.get("/login", function (req, res) {
     // If the user already has an account send them to the home page
     if (req.user) {
-      res.redirect("/allUsers");
+      res.redirect("/home");
     } else {
       res.render("login");
     }
@@ -41,7 +39,7 @@ module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  // app.get("/home", isAuthenticated, function (req, res) {
-  //   res.render("signup");
-  // });
+//   app.get("/home", isAuthenticated, function (req, res) {
+//     res.render("signin");
+//   });
 };
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const passport = require("passport");
-const SteamStrategy = require("passport-steam").Strategy;
+// const SteamStrategy = require("passport-steam").Strategy;
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 
 const db = require("./models");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,31 +23,31 @@ app.set("view engine", "handlebars");
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-passport.use(
-  new SteamStrategy(
-    {
-      // returnURL: "http://localhost:3000/auth/steam/return",
-      // realm: "http://localhost:3000/",
-      returnURL: "https://gamers-alike.herokuapp.com/auth/steam/return",
-      realm: "https://gamers-alike.herokuapp.com/",
-      apiKey: "D94580D5312585B718FA616F6F1CB4F0",
-    },
-    function (identifier, profile, done) {
-      process.nextTick(function () {
-        profile.identifier = identifier;
-        return done(null, profile);
-      });
-    }
-  )
-);
+// passport.use(
+//   new SteamStrategy(
+//     {
+//       // returnURL: "http://localhost:3000/auth/steam/return",
+//       // realm: "http://localhost:3000/",
+//       returnURL: "https://gamers-alike.herokuapp.com/auth/steam/return",
+//       realm: "https://gamers-alike.herokuapp.com/",
+//       apiKey: "D94580D5312585B718FA616F6F1CB4F0",
+//     },
+//     function (identifier, profile, done) {
+//       process.nextTick(function () {
+//         profile.identifier = identifier;
+//         return done(null, profile);
+//       });
+//     }
+//   )
+// );
 
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
+// passport.serializeUser(function (user, done) {
+//   done(null, user);
+// });
 
-passport.deserializeUser(function (obj, done) {
-  done(null, obj);
-});
+// passport.deserializeUser(function (obj, done) {
+//   done(null, obj);
+// });
 
 app.use(
   session({
@@ -59,19 +59,19 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", function (req, res) {
-  if (req.user)
-    res.send(
-      "Stored in session when logged : <br><br> " +
-        JSON.stringify(req.user) +
-        "<br><br>" +
-        '<a href="/logout">Logout</a>'
-    );
-  else
-    res.send(
-      'Not connected : <a href="/auth/steam"><img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_small.png"></a>'
-    );
-});
+// app.get("/", function (req, res) {
+//   if (req.user)
+//     res.send(
+//       "Stored in session when logged : <br><br> " +
+//         JSON.stringify(req.user) +
+//         "<br><br>" +
+//         '<a href="/logout">Logout</a>'
+//     );
+//   else
+//     res.send(
+//       'Not connected : <a href="/auth/steam"><img src="https://steamcommunity-a.akamaihd.net/public/images/signinthroughsteam/sits_small.png"></a>'
+//     );
+// });
 
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
